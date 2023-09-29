610c250e012803f2a8446684a785c40011b0380c
Matsu102 <124568793+Matsu102@users.noreply.github.com>
2023-09-29 13:01:19 +0000

    [Save]基本情報技術者、JavaScript

---
+++ JavaScript/app2.js
@@ -0,0 +1,12 @@
+let orange = 100;
+let apple = 120;
+
+// let ～させるの意味　orangeにさせるものは100？
+
+if(orange < apple){
+  alert('みかんの値段がリンゴより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
